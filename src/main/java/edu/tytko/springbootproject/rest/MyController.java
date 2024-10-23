cd486cf37c39709fbb84ad847eb4def7b2f6216d
Oleksandr Tytko <oleksandr.tytko@asd.team>
2024-10-23 21:24:32 +0300

    Test

--- src/main/java/edu/tytko/springbootproject/rest/MyController.java
+++ src/main/java/edu/tytko/springbootproject/rest/MyController.java
@@ -1,9 +1,11 @@
 package edu.tytko.springbootproject.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class MyController {
 
     @GetMapping
